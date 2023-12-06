a77fd2f24f752173a1da6087cda5e58c120d7bd0
Irulik <123405063+Irulik@users.noreply.github.com>
2023-12-06 23:12:38 +0100

    updateď Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,41 @@
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const headerStyle = {
+    backgroundColor: "white", 
+    color: "black",   
+  };
+
   return (
-    <nav className="navbar bg-dark mb-3 navbar-expand-lg">
+    <nav className="navbar mb-3 navbar-expand-lg" style={headerStyle}>
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1 text-success">Navbar</span>
-        
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink 
-              className="nav-link text-white" 
-              aria-current="page" 
-              to="/">
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link text-white" to="/catalog">
-                Catalog
-              </NavLink>
-            </li>
-            <li className="nav-item">
-               <NavLink className="nav-link text-white" to="/favorites">
-                Favorites
-              </NavLink>
-            </li>
-          </ul>
+        <span className="navbar-brand mb-0 h1 text-success">
+          Car rental
+        </span>
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item me-4">
+                <NavLink 
+                  className="nav-link fs-5" 
+                  aria-current="page" 
+                  to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item me-4">
+                <NavLink className="nav-link fs-5" to="/catalog">
+                  Catalog
+                </NavLink>
+              </li>
+              <li className="nav-item me-4">
+                <NavLink className="nav-link fs-5" to="/favorites">
+                  Favorites
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <hr style={{ margin: "0" }} /> {/* Лінія hr */}
         </div>
       </div>
     </nav>
